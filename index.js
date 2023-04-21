e8a91bc7f26a2460ed854849ac8e45f65fe45980
soniadiwedi <112754761+soniadiwedi@users.noreply.github.com>
2023-04-21 10:31:51 +0530

    first

--- index.js
+++ index.js
@@ -1,30 +1,16 @@
-const jsonServer = require("json-server");
-const cors = require("cors");
-const path = require("path");
+const jsonServer=require("json-server");
+const cors=require("cors");
+const path=require("path");
 
-const server = jsonServer.create();
-const router = jsonServer.router(path.join(__dirname, "db.json"));
-const middlewares = jsonServer.defaults();
+const server=jsonServer.create();
+const router=jsonServer.router(path.join(__dirname,"db.json"));
+const middlewares=jsonServer.defaults();
 
 server.use(cors());
 server.use(jsonServer.bodyParser);
 server.use(middlewares);
 server.use(router);
 
-const PORT = 8000;
-
-server.listen(PORT, () => {
-  console.log(`JSON Server is running on http://localhost:${PORT}`);
-});
-
-// // server.js
-// const jsonServer = require('json-server')
-// const server = jsonServer.create()
-// const router = jsonServer.router('db.json')
-// const middlewares = jsonServer.defaults()
-
-// server.use(middlewares)
-// server.use('', router)
-// server.listen(process.env.PORT || 5000, () => {
-//   console.log('JSON Server is running')
-// })
+server.listen(3000,()=>{
+    console.log("Server is running at http://localhost:3000");
+})
